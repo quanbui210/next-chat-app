0f0c99f7c7ec7ac174cc9f85ed808e038bb4bad2
quan.bui <quan.bui@dreambroker.fi>
2024-11-07 09:54:41 +0200

    setup ui

---
+++ components/ui/ChatHeader.tsx
@@ -0,0 +1,54 @@
+'use client';
+import React from 'react';
+import { Button } from './button';
+import { createClient } from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
+
+import { User } from '@supabase/supabase-js';
+
+interface ChatHeaderProps {
+  user?: User;
+}
+
+export default function ChatHeader({ user }: ChatHeaderProps) {
+  const router = useRouter();
+
+  const handleLoginWithGitHub = async () => {
+    try {
+      const supabase = createClient();
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: 'github',
+        options: {
+          redirectTo: location.origin + '/auth/callback',
+        },
+      });
+      if (error) console.error('Login error:', error.message);
+    } catch (err) {
+      console.error('Unexpected error:', err);
+    }
+  };
+
+  const handleLogout = async () => {
+    const supabase = createClient();
+
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return (
+    <div className="h-20">
+      <div className="p-5 border-b flex items-center justify-between h-full">
+        <div>
+          <h1 className="text-xl font-bold">Daily Chat</h1>
+          <div className="flex items-center gap-1">
+            <div className="h-3 w-3 bg-green-500 rounded-full animate-pulse"></div>
+            <h3 className="text-sm text-gray-400">{user?.email}</h3>
+          </div>
+        </div>
+        <Button onClick={user ? handleLogout : handleLoginWithGitHub}>
+          {user ? 'Log out' : 'Login'}
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ components/ui/ChatInput.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { Input } from '@/components/ui/input';
+import { createClient } from '@/utils/supabase/client';
+import React from 'react';
+import { toast } from 'sonner';
+
+export default function ChatInput() {
+  const supabase = createClient()
+  const handleSendMessage = async (text: string) => {
+    const {error} = await supabase.from("messages").insert({text})
+    if (error) {
+      toast.error(error.message)
+    }
+  }
+  return (
+    <div className="p-5">
+      <Input placeholder="send message" onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          handleSendMessage(e.currentTarget.value)
+          e.currentTarget.value = ""
+        }
+      }}/>
+    </div>
+  );
+}
